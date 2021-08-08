eb523c9f6d5b2caa3250ce1b913246ebc4c90579
raulsposito <raulsposito@gmail.com>
2021-08-08 20:46:12 -0300

    add nav footer and landing with tailwind color config

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-sans min-h-screen flex flex-col">
+    <Nav />
+    <main class="flex-grow">
+      <Nuxt />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      colors: {
+        primary: '#b6f0f2',
+        secondary: '#547a7b',
+        // ...
+      }
+    }
+  }
